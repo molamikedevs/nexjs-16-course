b7b2b02b694715e7f43de244ec9c0a63159caac8
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-11-01 01:31:56 +0400

    Initial commit

---
+++ config/font.ts
@@ -0,0 +1,11 @@
+import { Inter, Jost } from 'next/font/google'
+
+export const geistSans = Inter({
+	subsets: ['latin'],
+	variable: '--font-geist-sans',
+})
+
+export const geistMono = Jost({
+	subsets: ['latin'],
+	variable: '--font-geist-mono',
+})
